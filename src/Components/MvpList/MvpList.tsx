b1d9a1ce74ff41f5a717f9e9a739917f4566d704
MrKitey <60539960+TekiDev42@users.noreply.github.com>
2024-09-20 17:50:35 +0200

    add new components
    Add news components for pagination and Mvps list

---
+++ src/Components/MvpList/MvpList.tsx
@@ -0,0 +1,33 @@
+import style from "@/App.module.css";
+import {MvpCard} from "@components/MvpCard/MvpCard.tsx";
+import {useEffect, useState} from "react";
+import {getSortedMvp} from "@utils/getSortedMvp.ts";
+import {setMvps} from "@store/Slice/Mvp/Slice.ts";
+import {useAppDispatch, useAppSelector} from "@store/Hooks"
+import {createChunk} from "@utils/createChunk.ts";
+
+export const MvpList = () => {
+    const dispatch = useAppDispatch()
+    const mvps = useAppSelector(state => state.Slice.filtered)
+    const perPage = useAppSelector(state => state.userSlice.perPage)
+    const [activePage] = useState(1)
+    const data = createChunk(mvps.map(mvp => mvp), perPage)
+
+    useEffect(() => {
+        (async () => {
+            const mvps = await getSortedMvp()
+            dispatch(setMvps(mvps))
+        })()
+    }, [dispatch]);
+
+    const items = data.length ? data[activePage - 1].map((mvp, i) => (
+        <MvpCard key={`mvp-card-${i}`} mvp={mvp}/>
+    )) : []
+
+    return (
+        <div className={style.cardContainer}
+             style={{width: "100%"}}>
+            {items.map(item => item)}
+        </div>
+    )
+}
