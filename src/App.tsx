b70e8abdd03bc643191161ab59fd3d0bac4ba4e5
MrKitey <60539960+TekiDev42@users.noreply.github.com>
2024-09-21 17:51:36 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,21 @@ import {DeathFormModal} from "@components/Form/DeathForm"
 import {Background} from "@components/Background/Background"
 import {SelectPagination} from "@components/Pagination/SelectPagination.tsx";
 import {MvpList} from "@components/MvpList/MvpList.tsx";
+import {useEffect} from "react";
+import {setSettings} from "@store/Slice/User/UserSlice.ts";
+import {useAppDispatch} from "@store/Hooks.ts";
 
 
 const App = () => {
+    const dispatch = useAppDispatch()
+    
+    useEffect(() => {
+        (async () => {
+            const settings = await window.mvpApi.getSettings()
+            dispatch(setSettings(settings))
+        })()
+    }, [dispatch]);
+
     return (
         <>
             <div className={style.App}>
--- src/Components/Pagination/SelectPagination.tsx
+++ src/Components/Pagination/SelectPagination.tsx
@@ -1,20 +1,12 @@
 import {Pagination, Select} from "@mantine/core";
-import {setActivePage, setPerPage, setSettings} from "@store/Slice/User/UserSlice.ts";
+import {setActivePage, setPerPage} from "@store/Slice/User/UserSlice.ts";
 import {useAppDispatch, useAppSelector} from "@store/Hooks.ts";
-import {useEffect} from "react";
 
 export const SelectPagination = () => {
     const dispatch = useAppDispatch()
     const activePage = useAppSelector(state => state.userSlice.activePage)
     const perPage = useAppSelector(state => state.userSlice.perPage)
     const mvps = useAppSelector(state => state.Slice.filtered)
-
-    useEffect(() => {
-        (async () => {
-            const settings = await window.mvpApi.getSettings()
-            dispatch(setSettings(settings))
-        })()
-    }, [dispatch]);
 
     return (
         <>
