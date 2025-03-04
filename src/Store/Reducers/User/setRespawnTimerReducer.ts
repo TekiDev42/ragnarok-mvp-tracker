c690215f63fdf471ae992c36d21b1a7fb683a448
MrKitey <60539960+TekiDev42@users.noreply.github.com>
2025-03-04 18:25:07 +0100

    Update setRespawnTimerReducer.ts

--- src/Store/Reducers/User/setRespawnTimerReducer.ts
+++ src/Store/Reducers/User/setRespawnTimerReducer.ts
@@ -1,36 +1,13 @@
 import {PayloadAction} from "@reduxjs/toolkit"
-
+import { updateStorage } from "@utils/Storage/updateStorage.ts"
 /**
  * Reducer function to update the respawn timer setting in the user state.
  * 
  * This function updates the respawn timer setting in the state and persists
  * the change using the mvpApi.
- * 
- * @param state - The current UserState.
- * @param action - The action containing the new respawn timer value.
- * 
- * @example
- * // Assuming we have a current state with a respawn timer of 0:
- * const currentState = {
- *   respawnTimer: 0,
- *   // ... other user settings
- * }
- * 
- * // To change the respawn timer to 5 minutes:
- * const action = {
- *   type: 'user/setRespawnTimer',
- *   payload: 5
- * }
- * 
- * setRespawnTimerReducer(currentState, action)
- * // This will update the state to:
- * // {
- * //   respawnTimer: 5,
- * //   // ... other user settings remain unchanged
- * // }
- * // And also update this setting in the persistent storage using window.mvpApi.setSettings()
  */
 export const setRespawnTimerReducer = (state: UserState, action: PayloadAction<number>) => {
     state.respawnTimer = action.payload
     window.mvpApi.setSettings('respawnTimer', action.payload)
+    updateStorage('respawnTimer', action.payload)
 }
