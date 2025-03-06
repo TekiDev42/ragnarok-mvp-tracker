74332007e7b82abbbd84a9035a99dc241258c796
MrKitey <60539960+TekiDev42@users.noreply.github.com>
2025-03-06 17:50:27 +0100

    Update index.ts

--- import-database/src/index.ts
+++ import-database/src/index.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
+import { defaultMvps as mvps } from './mvps'
 
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_ANON_KEY
