70f80e94f90aca7b06128675aca078fba1e6cadd
MrKitey <60539960+TekiDev42@users.noreply.github.com>
2025-03-21 11:30:18 +0100

    Update SubscriptionsSupabase.tsx

--- src/Components/SubscriptionsSupabase/SubscriptionsSupabase.tsx
+++ src/Components/SubscriptionsSupabase/SubscriptionsSupabase.tsx
@@ -70,7 +70,11 @@ export const UseSubscriptionsSupabase = () => {
             }
 
             notifications.show({
-                title: <div className="text-gray-500 text-xs italic">Updated by <Badge leftSection={<IconUserCircle size={16} />} autoContrast size="sm" color={color}>{pseudo}</Badge></div>,
+                title: <div className="flex gap-1 items-center text-gray-500 text-xs italic"><span>Updated by</span>
+                            <Badge leftSection={<IconUserCircle size={16} />} autoContrast size="sm" color={color}>
+                                {pseudo}
+                            </Badge>
+                        </div>,
                 message: <Flex direction="column" gap={0}>
                     <div className="text-gray-500 text-xs italic">Respawn : {DateTime.fromMillis(payload.new.death_time).toFormat("dd/MM/yyyy HH'h'mm")}</div>
                     <div className="text-gray-800 text-md font-bold hover:text-blue-500">
