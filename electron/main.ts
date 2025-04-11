53ea24209f62272fc1cc9f6c738e718493215e54
MrKitey <60539960+TekiDev42@users.noreply.github.com>
2025-04-11 18:01:04 +0200

    v1.0.28
    Use autoUpdater from electron-builder

--- electron/main.ts
+++ electron/main.ts
@@ -1,12 +1,41 @@
+import { dialog } from "electron";
 import {MvpApp} from "./classes/MvpApp.ts";
-import {updateElectronApp, UpdateSourceType} from "update-electron-app";
+import { autoUpdater } from 'electron-updater';
 
-updateElectronApp({
-    updateSource: {
-        type: UpdateSourceType.ElectronPublicUpdateService,
-        repo: 'TekiDev42/ragnarok-mvp-tracker'
-    },
-    updateInterval: '10 minutes',
+
+autoUpdater.autoDownload = false
+
+autoUpdater.on('error', (error) => {
+    dialog.showErrorBox('Error: ', error == null ? "unknown" : (error.stack || error).toString())
+})
+
+autoUpdater.on('update-available', () => {
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Found Updates',
+        message: 'Found updates, do you want update now?',
+        buttons: ['Sure', 'No']
+    }).then((buttonIndex) => {
+        if (buttonIndex.response === 0) {
+            autoUpdater.downloadUpdate()
+        }
+    })
+})
+
+autoUpdater.on('update-not-available', () => {
+    dialog.showMessageBox({
+        title: 'No Updates',
+        message: 'Current version is up-to-date.'
+    })
+})
+
+autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+        title: 'Install Updates',
+        message: 'Updates downloaded, application will be quit for update...'
+    }).then(() => {
+        setImmediate(() => autoUpdater.quitAndInstall())
+    })
 })
 
 new MvpApp()
