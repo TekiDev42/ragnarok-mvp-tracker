d904a68ae41ecbf6149b107dfdb325c2ac6dd002
MrKitey <60539960+TekiDev42@users.noreply.github.com>
2025-04-11 13:56:44 +0200

    autoUpdater
    Test autoUpdater

--- electron/main.ts
+++ electron/main.ts
@@ -1,13 +1,31 @@
 import {MvpApp} from "./classes/MvpApp.ts";
 
-import {updateElectronApp, UpdateSourceType } from 'update-electron-app'
+import { app, autoUpdater, dialog } from 'electron'
 
-updateElectronApp({
-    updateSource: {
-        type: UpdateSourceType.ElectronPublicUpdateService,
-        repo: 'TekiDev42/ragnarok-mvp-tracker'
-    },
-    updateInterval: '1 hour',
+const server = 'https://github.com/TekiDev42/ragnarok-mvp-tracker'
+const feed = `${server}/releases/tag/v${app.getVersion()}`
+
+autoUpdater.setFeedURL({
+    url: feed
+})
+
+setInterval(() => {
+    autoUpdater.checkForUpdates()
+}, 10 * 60 * 1000)
+
+autoUpdater.on('update-downloaded', (_event, _releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: "info" as const,
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: `v${releaseName}`,
+      detail:
+        `Une nouvelle version a été téléchargée. Redémarrez l'application pour appliquer les mises à jour.`
+    }
+  
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+        if (returnValue.response === 0) autoUpdater.quitAndInstall()
+    })
 })
 
 new MvpApp()
