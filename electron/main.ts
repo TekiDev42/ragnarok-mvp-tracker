e167dcd919b1e6e65d360e63cd8cd53bc2893bec
MrKitey <60539960+TekiDev42@users.noreply.github.com>
2025-04-11 13:36:31 +0200

    update

--- electron/main.ts
+++ electron/main.ts
@@ -1,3 +1,13 @@
 import {MvpApp} from "./classes/MvpApp.ts";
 
+import {updateElectronApp, UpdateSourceType } from 'update-electron-app'
+
+updateElectronApp({
+    updateSource: {
+        type: UpdateSourceType.ElectronPublicUpdateService,
+        repo: 'TekiDev42/ragnarok-mvp-tracker'
+    },
+    updateInterval: '1 hour',
+})
+
 new MvpApp()
